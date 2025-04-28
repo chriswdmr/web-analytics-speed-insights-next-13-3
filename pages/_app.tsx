7540afbb1ef2672c12c0acca50b34646fb489f44
Chris <7249920+chriswdmr@users.noreply.github.com>
2025-04-28 14:46:21 +0200

    Use react import for Web Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
